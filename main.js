1fe58b5b462469022a7a8267c2d99c8ce1aa3b31
Achiengy <hope.pete@student.moringaschool.com>
2022-03-06 21:10:47 +0300

    ft:arrays-This commit will add arrays in the js file

---
+++ main.js
@@ -0,0 +1,27 @@
+const dayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const maleNames = ["Kwasi","Kwadwa","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ana"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let reset = function(){
+    location.reload();
+}
+
