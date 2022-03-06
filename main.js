2bf89454a1363dc6ac1643f377c4e59a97b3443f
Achiengy <hope.pete@student.moringaschool.com>
2022-03-06 21:33:06 +0300

    This commit will call a JavaScript function from the submit button

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const dayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const maleNames = ["Kwasi","Kwadwa","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ana"];
+const daysArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const maleAkanNames = ["Kwasi","Kwadwa","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ana"];
+
+let formValidation = function(){
+    HTMLVideoElement();
+    let dateData = document.form["form"]["date"].value
+    let genderData = document.form["form"]["gender"].value
+
+   
+}
 
 
 
