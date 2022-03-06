d2880318b3dca652c1947a8f81cd6fd7e6f6a5e6
Achiengy <hope.pete@student.moringaschool.com>
2022-03-06 22:05:29 +0300

    This commit will call the hide elements function

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const daysArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const maleAkanNames = ["Kwasi","Kwadwa","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-const femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ana"];
+let daysArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let maleAkanNames = ["Kwasi","Kwadwa","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+let femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ana"];
 
 let formValidation = function(){
-    HTMLVideoElement();
+    hideElement();
     let dateData = document.form["form"]["date"].value
     let genderData = document.form["form"]["gender"].value
 
-    if(dateData = "" || genderData = ""){
+    if(dateData == "" || genderData == ""){
         unhideElement();
-        alert("Enter valid information correctly")
+        alert("Enter valid information!")
         return false;
     }else{
         getUserDetails();
     }
+}
+
     let getUserDetails = function(){
         let date = document.getElementById("date").value;
         let gender = document.getElementById("gender").value;
@@ -23,16 +25,23 @@ let formValidation = function(){
     }
     let display = document.getElementById("display")
 
-    let getAkhanName = function(day,gender){
+    let getAkhanName = function(day, gender){
         if(gender === "Male"){
-            display.innerHTML = <p>Your Akan Name is <em>${maleAkanNames[day]}</em>because you are a ${gender}born on${daysArray[day]}</p>
+            display.innerHTML = <p>Your Akan Name is <em>${maleAkanNames[day]}</em>because you are a <em>${gender} </em>born on ${daysArray[day]}</p>
+
         }else if(gender === "Female"){
-            display.innerHTML = <p>Your Akan Name is <em>${femaleAkanNames[day]}</em>because you are a ${gender}born on${daysArray[day]}</p>
+            display.innerHTML = <p>Your Akan Name is <em>${femaleAkanNames[day]}</em>because you are a ${gender}born on ${daysArray[day]}</p>
         }
 
     }
-    let hideElement
-}
+
+    let hideElement = function(){
+        var x = document.getElementById("hidden-display");
+        x.style.display = "block";
+    }
+
+   
+
 
 
 
